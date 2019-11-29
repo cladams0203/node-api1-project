4e5609d9cdee21a83a38aba002da78fd53f8140f
cladams0203 <cladams0203@gmail.com>
2019-11-29 15:54:54 -0500

    added status codes for get getid and post

--- index.js
+++ index.js
@@ -5,30 +5,53 @@ const db = require('./data/db')
 
 const server = express() // created server
 
+server.use(express.json()) // so express uses json
+
 // handle requests to the root  of the api
 server.get('/', (req, res) => {
     res.send("hello")
 })
 
 // get to /users that returns the list of users
-server.get('/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     db.find()
         .then(users => {
-            res.json(users)
+            res.status(200).json(users)
         })
-        .catch(err => res.json({error: 'failed to get users'}))
+        .catch(err => res.status(500).json({ error: "The users information could not be retrieved.", error: err }))
 })
 // get to find a user by id
-server.get('/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     const id = req.params.id
+
     db.findById(id)
         .then(user => {
-            res.json(user)
+            user ?
+            res.status(200).json(user) :
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
         })
-        .catch(err => res.json({error: 'failed to get user'}))
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." , error: err})
+        })
+})
+// add user with the insert method of the db
+server.post('/api/users', (req, res) => {
+    const requestBody = req.body
+    const { name, bio } = req.body
+    if(!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    db.insert(requestBody)
+        .then(users => {
+            res.status(201).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "Please provide name and bio for the user.", error: err })
+        })
 })
 
 // watching for connections on port 5000
 server.listen(5000, () => {
     console.log('running on port 5000')
 })
+
