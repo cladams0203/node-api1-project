279e9686c491e800c61f6b548fdc04e790007d05
cladams0203 <cladams0203@gmail.com>
2019-11-29 21:21:14 -0500

    added form validation

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -3,7 +3,7 @@ import useForm from 'react-hook-form'
 import axios from 'axios'
 
 export function AddUser(props) {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, errors } = useForm()
     const onSubmit = data => {
         axios.post('http://localhost:5000/api/users', data)
             .then(res => {
@@ -12,13 +12,14 @@ export function AddUser(props) {
                 props.history.push('/')
             })
             .catch(err => console.log(err))
-        console.log(data)
     }
 
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
             <input name='name' placeholder='Name' ref={register({required: true})} />
+            {errors.name && 'Name is Required'}
             <input name='bio' placeholder='Bio' ref={register({required: true})} />
+            {errors.bio && 'Bio is Required'}
             <button type='submit'>Add User</button>
         </form>
     )
